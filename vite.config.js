30c890b1c8e41a39563e59fdac7bcd50d153e06d
craftxbox <craftxbox@craftxbox.com>
2025-05-27 21:46:53 -0230

    fix having to manually reset the /assets/ dir for my deployments

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    publicDir: 'corru-editor'
-})
+    build: {
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split(".").at(1);
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = "img";
+                    }
+                    return `corru-editor/assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: "corru-editor/assets/js/[name]-[hash].js",
+                entryFileNames: "corru-editor/assets/js/[name]-[hash].js",
+            },
+        },
+    },
+});
