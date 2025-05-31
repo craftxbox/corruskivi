318427de1829b33c1409a1400cb9c071e742a330
craftxbox <craftxbox@craftxbox.com>
2025-05-31 08:18:57 -0230

    actor voices custom howls

--- src/customactors.ts
+++ src/customactors.ts
@@ -2,6 +2,7 @@ import { clone } from "./clone";
 import * as monaco from "monaco-editor";
 import { generateEditorDialogue } from "./main";
 import { insertExtraActors } from "./actors";
+import { fetchHowl } from "./howl";
 
 const editorContainer = document.getElementById("customactors") as HTMLElement;
 const actorsEditor = monaco.editor.create(editorContainer, {
@@ -32,8 +33,14 @@ for (let key of Object.keys(window.env.dialogueActors)) {
 let originalActors = clone(window.env.dialogueActors);
 
 function parseActors(actors: string) {
-    let actorJSON = actors.replaceAll(/\( *\) *=> *play *\( *['"`](.*)['"`] *, *(\d+\.?\d*) *\)/g, (_, sound, volume) => {
-            return `["${sound}", ${volume}]`;
+    let actorJSON = actors.replaceAll(/\( *\) *=> *play *\( *['"`](.*)['"`] *, *(\d+\.?\d*) *\)/g, (_, sound, rate) => {
+            return `["${sound}", ${rate}]`;
+    });
+
+    console.log(actorJSON.match(/\(\) => {(\w+).rate\((\d+\.?\d*)\);\w+.play\((".+")?\)}/g))
+
+    actorJSON = actorJSON.replaceAll(/\(\) => {(\w+).rate\((\d+\.?\d*)\);\w+.play\((".+")?\)}/g, (_,name, rate, sound) => {
+            return `["${name}", "${sound || "__default"}", ${rate || "1"}]`;
     });
 
     return JSON.parse(actorJSON);
@@ -59,6 +66,16 @@ export function updateActors() {
                 if (voice?.length == 2) {
                     window.env.dialogueActors[actorName].voice = () => {
                         window.play(voice[0], voice[1]);
+                    };
+                } else if(voice?.length == 3) {
+                    window.env.dialogueActors[actorName].voice = () => {
+                        let howl = fetchHowl(voice[0]);
+                        if (!howl) {
+                            console.error(`Howl "${voice[0]}" does not exist.`);
+                            return;
+                        }
+                        howl.rate(voice[2]);
+                        howl.play(voice[1].length > 0 ? voice[1] : undefined);
                     };
                 }
             }
