d8207205372867f5716146601ee60a138dd5c472
craftxbox <craftxbox@craftxbox.com>
2025-06-22 05:08:35 -0230

    editor will now try to autocomplete keywords, actors, annotations, etc

--- src/monaco.ts
+++ src/monaco.ts
@@ -1,4 +1,5 @@
 import * as monaco from "monaco-editor";
+import { type IRange } from "monaco-editor";
 
 const editorContainer = document.getElementById("editor-text") as HTMLElement;
 
@@ -46,7 +47,7 @@ monaco.languages.registerFoldingRangeProvider("corru-dialogue", {
 
         // fold branches
         for (let i = 0; i < model.getLineCount(); i++) {
-            if (i === (model.getLineCount() - 1) && startLine !== -1) {
+            if (i === model.getLineCount() - 1 && startLine !== -1) {
                 endLine = i + 1; // Last line of the file
                 ranges.push({
                     start: startLine,
@@ -91,7 +92,7 @@ monaco.languages.registerFoldingRangeProvider("corru-dialogue", {
         for (let i = 0; i < model.getLineCount(); i++) {
             const lineText = model.getLineContent(i + 1);
 
-            if (i === (model.getLineCount() - 1) && startLine !== -1) {
+            if (i === model.getLineCount() - 1 && startLine !== -1) {
                 endLine = i + 1; // Last line of the file
                 ranges.push({
                     start: startLine,
@@ -115,7 +116,6 @@ monaco.languages.registerFoldingRangeProvider("corru-dialogue", {
                     startLine = i + 1; // Reset for the next block
                 }
             }
-
         }
 
         // fold SHOWIFs
@@ -124,7 +124,7 @@ monaco.languages.registerFoldingRangeProvider("corru-dialogue", {
         for (let i = 0; i < model.getLineCount(); i++) {
             const lineText = model.getLineContent(i + 1);
 
-            if (i === (model.getLineCount() - 1) && startLine !== -1) {
+            if (i === model.getLineCount() - 1 && startLine !== -1) {
                 endLine = i + 1; // Last line of the file
                 ranges.push({
                     start: startLine,
@@ -161,7 +161,7 @@ monaco.languages.registerFoldingRangeProvider("corru-dialogue", {
                         break;
                     }
 
-                    if (j === (model.getLineCount() - 1)) {
+                    if (j === model.getLineCount() - 1) {
                         endLine = j + 1; // Last line of the file
                         ranges.push({
                             start: startLine,
@@ -176,6 +176,362 @@ monaco.languages.registerFoldingRangeProvider("corru-dialogue", {
 
         return ranges;
     },
+});
+
+function createAnnotationProposals(range: IRange) {
+    return [
+        {
+            label: "@name",
+            kind: monaco.languages.CompletionItemKind.Property,
+            documentation: "Declare a new dialogue chain",
+            insertText: "@name ",
+            range: range,
+        },
+        {
+            label: "@respobj",
+            kind: monaco.languages.CompletionItemKind.Property,
+            documentation: "Declare a new response object",
+            insertText: "@respobj ",
+            range: range,
+        },
+        {
+            label: "@testpath",
+            kind: monaco.languages.CompletionItemKind.Property,
+            documentation: "Set a path for the test button to follow",
+            insertText: "@testpath ",
+            range: range,
+        },
+        {
+            label: "@background",
+            kind: monaco.languages.CompletionItemKind.Property,
+            documentation: "Set the background image for the dialogue",
+            insertText: "@background ",
+            range: range,
+        },
+        {
+            label: "@foreground",
+            kind: monaco.languages.CompletionItemKind.Property,
+            documentation: "Set the foreground image for the dialogue",
+            insertText: "@foreground ",
+            range: range,
+        },
+    ];
+}
+
+function createRootCmdProposals(range: IRange) {
+    return [
+        {
+            label: "____SHOWIF::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Show this dialogue only if the condition is met",
+            insertText: '____SHOWIF::[["fbx__${1:editorpreview}_${2:branch}"]]',
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+        },
+        {
+            label: "____NESTIF::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Start a nested condition block",
+            insertText: '____NESTIF::[["fbx__${1:editorpreview}_${2:branch}"]]',
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+        },
+        {
+            label: "____END",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "End the current dialogue block",
+            insertText: "____END",
+            range: range,
+        },
+    ];
+}
+
+function createActorProposals(range: IRange, response = false) {
+    let proposals = [];
+    for (let actor in window.env.dialogueActors) {
+        if (window.env.dialogueActors.hasOwnProperty(actor)) {
+            proposals.push({
+                label: (response ? "RESPONSES::" : "") + actor,
+                kind: monaco.languages.CompletionItemKind.Class,
+                documentation: `Dialogue actor: ${actor}`,
+                insertText: (response ? "RESPONSES::" : "") + actor + "\n    ",
+                range: range,
+                preselect: actor === "self",
+            });
+        }
+    }
+    return proposals;
+}
+
+function createCommandProposals(range: IRange) {
+    return [
+        {
+            label: "AUTOADVANCE::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Automatically advance to the next dialogue once this one is shown",
+            insertText: "AUTOADVANCE::",
+            range: range,
+        },
+        {
+            label: "WAIT::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Wait for a certain amount of milliseconds before continuing",
+            insertText: "WAIT::${1:1000}",
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+        },
+        {
+            label: "CLASS::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Set a CSS class for the dialogue box",
+            insertText: "CLASS::${1:classname}",
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+        },
+        {
+            label: "SHOWONCE::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Show this dialogue only once per session",
+            insertText: "SHOWONCE::",
+            range: range,
+        },
+        {
+            label: "SHOWIF::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Show this dialogue only if the condition is met",
+            insertText: 'SHOWIF::[["fbx__${1:editorpreview}_${2:branch}"]]',
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+        },
+    ];
+}
+
+function createExecCommandProposals(range: IRange) {
+    return [
+        {
+            label: "EXEC::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Execute javascript immediately",
+            insertText: "EXEC::",
+            range: range,
+        },
+        {
+            label: "END::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Execute javascript when the dialogue ends",
+            insertText: "END::",
+            range: range,
+        },
+        {
+            label: "THEN::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Execute javascript after a WAIT:: completes",
+            insertText: "THEN::",
+            range: range,
+        },
+        {
+            label: "SKIP::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Add a skip button and execute javascript when clicked",
+            insertText: "SKIP::",
+            range: range,
+        },
+        {
+            label: "SKIPNOTICE::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Set the warning notice for the skip button",
+            insertText: "SKIPNOTICE::${1:Are you sure you want to skip this dialogue?}",
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+        },
+        {
+            label: "SKIPTIME::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Set the time in milliseconds the skip overlay will be shown",
+            insertText: "SKIPTIME::${1:1000}",
+            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+            range: range,
+        },
+    ];
+}
+
+function createResponseProposals(model: monaco.editor.ITextModel, range: IRange) {
+    let proposals: any[] = [];
+    let execCheck = document.getElementById("enable-exec") as HTMLInputElement;
+    if (execCheck.checked) {
+        proposals.push({
+            label: "UNREADCHECK::",
+            kind: monaco.languages.CompletionItemKind.Keyword,
+            documentation: "Execute javascript to see if the response will be shown as unread",
+            insertText: "UNREADCHECK::",
+            range: range,
+        });
+    }
+
+    for (let i = 0; i < model.getLineCount(); i++) {
+        const lineText = model.getLineContent(i + 1);
+        let match = lineText.match(/^([a-zA-Z0-9]\w+)/);
+        if (match) {
+            proposals.push({
+                label: match[1],
+                kind: monaco.languages.CompletionItemKind.Variable,
+                documentation: `Branch: ${match[1]}`,
+                insertText: match[1] + "\n    ",
+                range: range,
+            });
+        }
+
+        match = lineText.match(/^\x40name (.+)/);
+        if (match) {
+            proposals.push({
+                label: "CHANGE::" + match[1],
+                kind: monaco.languages.CompletionItemKind.Class,
+                documentation: `Dialogue chain: ${match[1]}`,
+                insertText: "CHANGE::" + match[1] + "\n    ",
+                range: range,
+            });
+        }
+    }
+
+    return proposals;
+}
+
+function createRespobjProposals(model: monaco.editor.ITextModel, range: IRange) {
+    let proposals = [];
+    for (let i = 0; i < model.getLineCount(); i++) {
+        const lineText = model.getLineContent(i + 1);
+        let match = lineText.match(/^@respobj (.+)/);
+        if (match) {
+            proposals.push({
+                label: "RESPOBJ::" + match[1],
+                kind: monaco.languages.CompletionItemKind.Variable,
+                documentation: `Response object: ${match[1]}`,
+                insertText: "RESPOBJ::" + match[1] + "\n    ",
+                range: range,
+            });
+        }
+    }
+    return proposals;
+}
+
+monaco.languages.registerCompletionItemProvider("corru-dialogue", {
+    provideCompletionItems: function (model, position) {
+        let line = model.getLineContent(position.lineNumber);
+
+        let indentationCount = 0;
+        while (line[indentationCount] === " ") {
+            indentationCount++;
+        }
+        let indentationLevel = indentationCount / 4;
+
+        let proposals = [];
+
+        let word = model.getWordUntilPosition(position);
+
+        let lastActor: string | undefined;
+
+        for (let i = position.lineNumber - 1; i > 0; i--) {
+            const previousLine = model.getLineContent(i);
+            if (previousLine.match(/^    \w/)) {
+                lastActor = previousLine.trim();
+                break;
+            }
+        }
+
+        switch (indentationLevel) {
+            case 0:
+                var range = {
+                    startLineNumber: position.lineNumber,
+                    endLineNumber: position.lineNumber,
+                    startColumn: 1,
+                    endColumn: word.endColumn,
+                };
+
+                if (line.startsWith("@")) {
+                    proposals.push(createAnnotationProposals(range));
+                } else if (line.startsWith("_")) {
+                    proposals.push(createRootCmdProposals(range));
+                } else {
+                    proposals.push([
+                        {
+                            label: "start",
+                            kind: monaco.languages.CompletionItemKind.Keyword,
+                            documentation: "Default branch for a chain",
+                            insertText: "start\n    ",
+                            range: range,
+                        },
+                    ]);
+                }
+
+                break;
+            case 1:
+                var range = {
+                    startLineNumber: position.lineNumber,
+                    endLineNumber: position.lineNumber,
+                    startColumn: word.startColumn,
+                    endColumn: word.endColumn,
+                };
+
+                if (word.word.match(/^RESPOB/i)) {
+                    proposals.push(createRespobjProposals(model, range));
+                } else if (word.word.match(/^RESP/i)) {
+                    proposals.push(createActorProposals(range, true));
+                } else {
+                    proposals.push(createActorProposals(range));
+                }
+                break;
+            case 2:
+                if (lastActor === undefined) break;
+
+                var range = {
+                    startLineNumber: position.lineNumber,
+                    endLineNumber: position.lineNumber,
+                    startColumn: word.startColumn,
+                    endColumn: word.endColumn,
+                };
+
+                if (lastActor.startsWith("RESPONSES::")) {
+                    if (line.includes("<+>")) {
+                        proposals.push(createResponseProposals(model, range));
+                    }
+                }
+
+                break;
+            case 3:
+                var range = {
+                    startLineNumber: position.lineNumber,
+                    endLineNumber: position.lineNumber,
+                    startColumn: word.startColumn,
+                    endColumn: word.endColumn,
+                };
+
+                if (lastActor?.startsWith("RESPONSES::")) {
+                    proposals.push([
+                        {
+                            label: "FAKEEND::",
+                            kind: monaco.languages.CompletionItemKind.Keyword,
+                            documentation: "Make the response button look like it ends the dialogue",
+                            insertText: "FAKEEND::${1:(back)}",
+                            insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                            range: range,
+                        },
+                    ]);
+                }
+
+                proposals.push(createCommandProposals(range));
+
+                let execCheck = document.getElementById("enable-exec") as HTMLInputElement;
+                if (execCheck.checked) {
+                    proposals.push(createExecCommandProposals(range));
+                }
+                break;
+        }
+
+        return {
+            suggestions: proposals.flat(),
+        };
+    },
+    triggerCharacters: ["@", "_", ":", ">"],
 });
 
 const corruEditor = monaco.editor.create(editorContainer, {
