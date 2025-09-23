784985a215b40dfc81a1f9166c2fdd2ab6a9093a
craftxbox <craftxbox@craftxbox.com>
2025-09-23 19:52:42 -0230

    fix wiki button

--- src/startup.ts
+++ src/startup.ts
@@ -103,7 +103,7 @@ start
             SHOWIF::[["ENV!!directFromUrl", false], ["ENV!!vaporwave", true]]
             HIDEREAD::
         credits<+>credits
-        wiki<+>EXEC::window.open("https://github.com/craftxbox/corruskivi/wiki", "_blank");
+        wiki<+>EXEC::window.open('https://github.com/craftxbox/corruskivi/wiki', '_blank');
         back<+>EXEC::window.enterDirectPreview();
             SHOWIF::[["ENV!!directFromUrl", true]]
             FAKEEND::(back)
@@ -122,7 +122,7 @@ options
             SHOWIF::[["ENV!!directFromUrl", false], ["ENV!!vaporwave", true]]
             HIDEREAD::
         credits<+>credits
-        wiki<+>EXEC::window.open("https://github.com/craftxbox/corruskivi/wiki", "_blank");
+        wiki<+>EXEC::window.open('https://github.com/craftxbox/corruskivi/wiki', '_blank');
         back<+>EXEC::window.enterDirectPreview();
             SHOWIF::[["ENV!!directFromUrl", true]]
             FAKEEND::(back)
