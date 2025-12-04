b812ff26c0c7c2ba390e2c9750babdc5170bfcb2
craftxbox <craftxbox@craftxbox.com>
2025-12-03 21:52:00 -0330

    tenative fix for strange monaco issue

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,21 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base:"/corruskivi/",
     build: {
         sourcemap: true,
         rollupOptions: {
             output: {
+                dir: "dist/corruskivi",
                 assetFileNames: (assetInfo) => {
                     let extType = assetInfo.name.split(".").at(1);
                     if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
                         extType = "img";
                     }
-                    return `corruskivi/assets/${extType}/[name]-[hash][extname]`;
+                    return `assets/${extType}/[name]-[hash][extname]`;
                 },
-                chunkFileNames: "corruskivi/assets/js/[name]-[hash].js",
-                entryFileNames: "corruskivi/assets/js/[name]-[hash].js",
+                chunkFileNames: "assets/js/[name]-[hash].js",
+                entryFileNames: "assets/js/[name]-[hash].js",
             },
         },
     },
