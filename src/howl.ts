0eea5806caa87aae4cb5f22b2ef451a1b852c3d8
craftxbox <craftxbox@craftxbox.com>
2025-12-27 03:34:32 -0330

    support for ratween

--- src/howl.ts
+++ src/howl.ts
@@ -21,7 +21,7 @@ export function preprocessHowls(dialogue: string, silent = false) {
     if (!execs) return dialogue; // no exec matches so we have nothing to do
 
     for (let exec of execs) {
-        if (!/play|pause|stop|mute|volume|fade|rate|seek|loop|load|unload/.test(exec)) continue; // this exec has no code we're interested in.
+        if (!/play|pause|stop|mute|volume|fade|rate|seek|loop|load|unload|ratween/.test(exec)) continue; // this exec has no code we're interested in.
 
         let howlObjects: { name: string; command: string; args: string }[] = [];
         let newExec = exec;
@@ -42,32 +42,61 @@ export function preprocessHowls(dialogue: string, silent = false) {
 
             let howl = howls[howlName];
             if (!howl) {
-                if (!silent) window.chatter({ actor: "funfriend", text: `Dialogue referenced a howl that does not exist! This will be ignored.`, readout: true });
+                if (!silent)
+                    window.chatter({ actor: "funfriend", text: `Dialogue referenced a howl that does not exist! This will be ignored.`, readout: true });
                 continue;
             } else {
                 howlObjects.push(howlObject);
             }
         }
 
-        let sfxmapCommands = exec.matchAll(/[^.]play\((.*)\)/g);
+        let sfxmapCommands = exec.matchAll(/[^.](play|ratween)\((.*)\)/g);
 
         if (sfxmapCommands) {
             for (let sfxmapCommand of sfxmapCommands) {
-                let [_, strArgs] = sfxmapCommand;
+                let [_, command, strArgs] = sfxmapCommand;
 
-                let args = [
-                    strArgs.split(",")[0].replaceAll("'", "").replaceAll('"', "").replaceAll("`", ""),
-                    parseFloat(strArgs.split(",")[1]) || true,
-                    parseFloat(strArgs.split(",")[2]) || 0.75,
-                ];
+                // I do not like this, but i did not feel like refactoring for a single extra command.
+                if (command === "ratween") {
+                    let howlName = strArgs.split(",")[0].replaceAll("'", "").replaceAll('"', "").replaceAll("`", "");
 
-                let howlObject = {
-                    name: "__SFXMAP",
-                    command: "play",
-                    args: JSON.stringify(args),
-                };
-                newExec = newExec.replace(sfxmapCommand[0].slice(1), "");
-                howlObjects.push(howlObject);
+                    let args = [parseFloat(strArgs.split(",")[1]) || 1.0, parseFloat(strArgs.split(",")[2]) || 1000];
+
+                    let howlObject = {
+                        name: howlName,
+                        command: "ratween",
+                        args: JSON.stringify(args),
+                    };
+
+                    let howl = howls[howlName];
+                    if (!howl) {
+                        if (!silent)
+                            window.chatter({
+                                actor: "funfriend",
+                                text: `Dialogue referenced a howl that does not exist! This will be ignored.`,
+                                readout: true,
+                            });
+                        continue;
+                    } else {
+                        newExec = newExec.replace(sfxmapCommand[0].slice(1), "");
+                        howlObjects.push(howlObject);
+                    }
+
+                } else if (command === "play") {
+                    let args = [
+                        strArgs.split(",")[0].replaceAll("'", "").replaceAll('"', "").replaceAll("`", ""),
+                        parseFloat(strArgs.split(",")[1]) || true,
+                        parseFloat(strArgs.split(",")[2]) || 0.75,
+                    ];
+
+                    let howlObject = {
+                        name: "__SFXMAP",
+                        command: "play",
+                        args: JSON.stringify(args),
+                    };
+                    newExec = newExec.replace(sfxmapCommand[0].slice(1), "");
+                    howlObjects.push(howlObject);
+                }
             }
         }
         if (howlObjects.length === 0) continue; // no howl commands found in this exec
@@ -98,7 +127,7 @@ export function postprocessHowls(dialogue: string) {
                 continue;
             }
 
-            if (!howlObject.command.match(/^(play|pause|stop|mute|volume|fade|rate|seek|loop|load|unload)$/)) {
+            if (!howlObject.command.match(/^(play|pause|stop|mute|volume|fade|rate|seek|loop|load|unload|ratween)$/)) {
                 window.chatter({ actor: "funfriend", text: `Dialogue referenced an invalid howl command! This will be ignored.`, readout: true });
                 continue;
             }
@@ -118,10 +147,20 @@ export function postprocessHowls(dialogue: string) {
                 let args = JSON.parse(`[${howlObject.args}]` || "[]");
                 args = JSON.stringify(args).slice(1, -1); // remove the surrounding brackets
 
-                newHowlCode += `fetchHowl("${howlObject.name}").${howlObject.command}(${args});`;
+                if (howlObject.command === "ratween") {
+                    args = args.slice(1, -1); // remove surrounding brackets again
+                    newHowlCode += `ratween(fetchHowl("${howlObject.name}"), ${args});`;
+                }
+                else {
+                    newHowlCode += `fetchHowl("${howlObject.name}").${howlObject.command}(${args});`;
+                }
             } catch (e) {
                 console.error("Failed to parse howl command arguments:", e);
-                window.chatter({ actor: "actual_site_error", text: `Howl command contains invalid arguments! It will be invoked without them.`, readout: true });
+                window.chatter({
+                    actor: "actual_site_error",
+                    text: `Howl command contains invalid arguments! It will be invoked without them.`,
+                    readout: true,
+                });
                 newHowlCode += `fetchHowl("${howlObject.name}").${howlObject.command}();`;
                 continue;
             }
@@ -181,7 +220,11 @@ export function updateHowls() {
             new Function(howlsContent);
         } catch (e) {
             console.error("Failed to evaluate howls content:", e);
-            window.chatter({ actor: "actual_site_error", text: "An error occurred while evaluating your howls. Please check the console for details.", readout: true });
+            window.chatter({
+                actor: "actual_site_error",
+                text: "An error occurred while evaluating your howls. Please check the console for details.",
+                readout: true,
+            });
             return 0;
         }
     }
